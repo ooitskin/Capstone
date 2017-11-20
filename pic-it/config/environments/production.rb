e58057a1921cbeb0b6ab2ac1834eaad57c6eaf8d
Kin L Lim <Lim.Kin@gmail.com>
2017-11-20 16:52:28 -0500

    some changes

--- pic-it/config/environments/production.rb
+++ pic-it/config/environments/production.rb
@@ -12,7 +12,8 @@ config.paperclip_defaults = {
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+  
+  config.serve_static_assets = true
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
