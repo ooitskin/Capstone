0620694b8a1bc30159acb17e32c57fff0f9eb869
Kin L Lim <Lim.Kin@gmail.com>
2017-11-20 15:56:00 -0500

    production changes

--- pic-it/config/environments/production.rb
+++ pic-it/config/environments/production.rb
@@ -3,10 +3,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
-    bucket: ENV.fetch('bucket'),
-    access_key_id: ENV.fetch("AWSAccessKeyId"),
-    secret_access_key: ENV.fetch("AWSSecretKey"),
-    s3_region: ENV.fetch('region'),
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch("AWS_ACCESS_KEY_ID"),
+    secret_access_key: ENV.fetch("AWS_SECRET_ACCESS_KEY"),
+    s3_region: ENV.fetch('AWS_REGION'),
   }
 }
 
